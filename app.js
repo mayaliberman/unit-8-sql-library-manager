20682b59655835bf9d37f0f86538cbba2768a245
Maya Liberman <50631141+mayaliberman@users.noreply.github.com>
2019-10-28 16:30:52 +0200

    rendering error

--- app.js
+++ app.js
@@ -41,8 +41,13 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error', { error: err });
+
+  if (err.status === 404) {
+    res.render('page-not-found')
+  } else {
+    res.status(err.status || 500);
+    res.render('error' );
+  }
 
 });
 
